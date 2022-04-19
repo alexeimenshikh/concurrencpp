96a510c001841f8e4c5588b63350d7ff626c4cd2
Aleksei Menshikh <amenshih@lanta-net.ru>
2022-04-19 09:38:25 +0300

    Вместо std::experimental оставил std; убрал ссылку на librt.

--- include/concurrencpp/coroutines/coroutine.h
+++ include/concurrencpp/coroutines/coroutine.h
@@ -3,9 +3,7 @@
 
 #include "../platform_defs.h"
 
-#ifdef CRCPP_MSVC_COMPILER
-
-#    include <coroutine>
+#include <coroutine>
 
 namespace concurrencpp::details {
     template<class promise_type>
@@ -13,18 +11,5 @@ namespace concurrencpp::details {
     using suspend_never = std::suspend_never;
     using suspend_always = std::suspend_always;
 }  // namespace concurrencpp::details
-
-#elif defined(CRCPP_CLANG_COMPILER)
-
-#    include <experimental/coroutine>
-
-namespace concurrencpp::details {
-    template<class promise_type>
-    using coroutine_handle = std::experimental::coroutine_handle<promise_type>;
-    using suspend_never = std::experimental::suspend_never;
-    using suspend_always = std::experimental::suspend_always;
-}  // namespace concurrencpp::details
-
-#endif
 
 #endif
